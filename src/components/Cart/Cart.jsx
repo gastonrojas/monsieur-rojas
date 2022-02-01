aea3c2633c5d9e4a22e4a8f277bcffb84262a619
tonga <grojas89@hotmail.com>
2022-02-01 17:39:21 -0300

    agregue orden

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import { useState } from 'react';
 import { useCartContext } from '../../context/cartContext';
 import { Link } from 'react-router-dom';
+import {
+  addDoc,
+  collection,
+  getFirestore,
+  Timestamp,
+} from 'firebase/firestore';
+import Resumen from './Resumen';
 
 function Cart() {
   const {
@@ -11,32 +18,74 @@ function Cart() {
     addOneItem,
     removeOneItem,
   } = useCartContext();
-console.log(cartList)
+  const [idOrden, setidOrden] = useState('');
+  const [condicional, setcondicional] = useState(false);
+
+  const realizarCompra = async (e) => {
+    e.preventDefault();
+
+    let orden = {};
+
+    orden.date = Timestamp.fromDate(new Date());
+    orden.buyer = {
+      name: 'Gaston',
+      email: 'grojas89@hotmail.com',
+      phone: 15612313211,
+    };
+    orden.total = totalPrice();
+    orden.items = cartList.map((cartItem) => {
+      const id = cartItem.id;
+      const nombre = cartItem.title;
+      const precio = cartItem.precio * cartItem.cantidad;
+      const cantidad = cartItem.cantidad;
+
+      return { id, nombre, precio, cantidad };
+    });
+
+    // guardar la orden en firestore
+    const db = getFirestore();
+
+    const oredenCollection = collection(db, 'ordenes');
+    await addDoc(oredenCollection, orden) // setDoc
+      .then((resp) => setidOrden(resp.id), setcondicional(true))
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <>
-      {cartList.length === 0 ? (
-        <div>
-          <h2>No hay items en su carrito... </h2>
-          <Link to="/">
-            <button>Ir a comprar!</button>
-          </Link>
-        </div>
+    <div>
+      {condicional ? (
+        <Resumen id={idOrden} />
       ) : (
-        <div className="cartContainer">
-          {cartList.map((prod) => (
-            <div key={prod.id}>
-              {prod.name} Precio: ARS {prod.price}. Cantidad: {prod.cantidad}.
-              <button onClick={() => addOneItem(prod.id, prod.stock)}>+</button>
-              <button onClick={() => removeOneItem(prod.id)}>-</button>
-              <button onClick={() => deleteItem(prod.id)}>Eliminar</button>
+        <>
+          {cartList.length === 0 ? (
+            <div>
+              <h2>No hay items en su carrito... </h2>
+              <Link to="/">
+                <button>Ir a comprar!</button>
+              </Link>
             </div>
-          ))}
-          <button onClick={() => vaciarCarrito()}>Vaciar carrito</button>
-          <h3>Total a pagar: ARS {totalPrice()}.</h3>
-        </div>
+          ) : (
+            <div className="cartContainer">
+              {cartList.map((prod) => (
+                <div key={prod.id}>
+                  {prod.name} Precio: ARS {prod.price}. Cantidad:{' '}
+                  {prod.cantidad}.
+                  <button onClick={() => addOneItem(prod.id, prod.stock)}>
+                    +
+                  </button>
+                  <button onClick={() => removeOneItem(prod.id)}>-</button>
+                  <button onClick={() => deleteItem(prod.id)}>Eliminar</button>
+                </div>
+              ))}
+              <button onClick={() => vaciarCarrito()}>Vaciar carrito</button>
+              <h3>Total a pagar: ARS {totalPrice()}.</h3>
+              <button onClick={realizarCompra}>Checkout</button>
+            </div>
+          )}
+        </>
       )}
-    </>
+    </div>
   );
 }
 
-export default Cart
+export default Cart;
---
+++ src/components/Cart/Resumen.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Resumen({id}) {
+  return <div>
+      El id de la orden generada es:  {id}
+  </div>;
+}
+
+export default Resumen;
